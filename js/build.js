18eb2dcb3b7b5340674ba532d5184caaf72934e8
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-12-13 11:57:26 +0000

    Tweaks

--- js/build.js
+++ js/build.js
@@ -1,9 +1,7 @@
 Fliplet.Widget.instance('inline-link', function (config) {
-  $('[data-inline-link-id="' + config.id + '"]').on('click', function (event) {
+  $(this).on('click', function (event) {
     event.preventDefault();
 
-    var data = Fliplet.Widget.getData($(this).data('inline-link-id'));
-
-    Fliplet.Navigate.to(data.action);
+    Fliplet.Navigate.to(config.action);
   });
 });
