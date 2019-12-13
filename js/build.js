6781159497097bee56e1176191e54089ed7e4e36
Hugo Carneiro <hello@thegeekdeveloper.com>
2019-12-13 11:53:42 +0000

    Use of Fliplet.Widget.instance()

--- js/build.js
+++ js/build.js
@@ -1,7 +1,9 @@
-$('[data-inline-link-id]').click(function (event) {
-  event.preventDefault();
+Fliplet.Widget.instance('inline-link', function (config) {
+  $('[data-inline-link-id="' + config.id + '"]').on('click', function (event) {
+    event.preventDefault();
 
-  var data = Fliplet.Widget.getData($(this).data('inline-link-id'));
+    var data = Fliplet.Widget.getData($(this).data('inline-link-id'));
 
-  Fliplet.Navigate.to(data.action);
+    Fliplet.Navigate.to(data.action);
+  });
 });
