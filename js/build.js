bc3a0ae7e6058b9bedbc608962481e9e2e3a4ba4
Maksym Shokin <maksym.sh@upplabs.com>
2020-08-03 15:27:35 +0300

    accessability

--- js/build.js
+++ js/build.js
@@ -1,6 +1,10 @@
 Fliplet.Widget.instance('inline-link', function (config) {
-  $(this).on('click', function (event) {
+  $(this).on('click keydown', function (event) {
     event.preventDefault();
+
+    if (event.type !== 'click' && event.keyCode !== 13 && event.keyCode !== 32) {
+      return;
+    }
 
     Fliplet.Navigate.to(config.action);
   });
