99ad7c19407e821897fd11518f19c09379cc5b23
Sofiia Kvasnevska <sofiia.k@upplabs.com>
2020-08-13 15:28:20 +0300

    Revert "[Fix] Accessibility"

--- js/build.js
+++ js/build.js
@@ -1,10 +1,6 @@
 Fliplet.Widget.instance('inline-link', function (config) {
-  $(this).on('click keydown', function (event) {
+  $(this).on('click', function (event) {
     event.preventDefault();
-
-    if (event.type !== 'click' && event.keyCode !== 13 && event.keyCode !== 32) {
-      return;
-    }
 
     Fliplet.Navigate.to(config.action);
   });
