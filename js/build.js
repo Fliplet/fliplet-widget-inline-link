eda10c7581c171b5a49763947301529c569df0bc
Tony Wu <tonytlwu@users.noreply.github.com>
2024-03-09 08:54:26 +0000

    ID-3827 Ensures inline links are only initialized once

--- js/build.js
+++ js/build.js
@@ -1,7 +1,11 @@
 Fliplet.Widget.instance('inline-link', function(config) {
+  if ($(this).data('initialized')) {
+    return;
+  }
+
   $(this).on('click', function(event) {
     event.preventDefault();
 
     Fliplet.Navigate.to(config.action);
-  });
+  }).data('initialized', true);
 });
