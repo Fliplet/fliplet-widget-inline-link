26e0a084041fda00eb632c1cbdb52a59409e9708
Ali Sufyan <alisufyan327@gmail.com>
2025-10-28 01:43:34 +0500

    ID-4728 [FEATURE] add dynamic context to Navigate.to action

--- js/build.js
+++ js/build.js
@@ -3,8 +3,15 @@ Fliplet.Widget.instance('inline-link', function(config) {
     return;
   }
 
-  $(this).on('click', function(event) {
+  $(this).on('click', async function(event) {
     event.preventDefault();
+    config.action = config.action || {};
+
+    try {
+      config.action.dynamicContext = await Fliplet.Widget.getDynamicContext($(this));
+    } catch (e) {
+      config.action.dynamicContext = {};
+    }
 
     Fliplet.Navigate.to(config.action);
   }).data('initialized', true);
